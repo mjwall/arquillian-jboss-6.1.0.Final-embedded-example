579873ace4329c12ef84768da24c26eba9d10310
Michael Wall <mjwall@gmail.com>
2013-09-03 14:38:00 -0400

    Holy crap, it ran
    And the first failure in the integration test was collected, but the
    tests moved on to finish the other test.  Sweet.
    
    Now to clean this up some

--- src/test/java/org/jboss/arquillian/examples/jbembedded/HelloEJBITCase.java
+++ src/test/java/org/jboss/arquillian/examples/jbembedded/HelloEJBITCase.java
@@ -6,16 +6,14 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.impl.base.ServiceExtensionLoader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * @author <a href="mailto:michaelschuetz83@gmail.com">Michael Schuetz</a>
- */
 @RunWith(Arquillian.class)
-public class HelloEJBITTest {
+public class HelloEJBITCase {
     @EJB
     private HelloEJB helloEJB;
 
@@ -23,15 +21,20 @@ public class HelloEJBITTest {
     public static JavaArchive createTestArchive() {
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "helloEJB.jar")
                 .addClasses(HelloEJB.class);
-        System.out.println("HERE MIKE");
-        System.out.println(jar.toString(true));
         return jar;
     }
 
     @Test
     public void testHelloEJB() {
         String result = helloEJB.sayHelloEJB("Michael");
+        System.out.println(result);
+        assertEquals("Hello Fred", result);
+    }
+
+    @Test
+    public void testHelloEJB2() {
+        String result = helloEJB.sayHelloEJB("Michael");
+        System.out.println(result);
         assertEquals("Hello Michael", result);
-        //assertEquals("Hello David", result);
     }
 }
